9df6bed7ac67bc0398a4c10921ffc95565f57fbb
Ian <new@news-MacBook-Pro.local>
2022-02-18 00:47:57 -0700

    updated footer with icons and href, styled projects and added 2 more projects

--- react-portfolio/src/components/Footer/Footer.js
+++ react-portfolio/src/components/Footer/Footer.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "../../App.css";
 import { DiGithubBadge } from "react-icons/di";
+import { AiFillLinkedin } from "react-icons/ai";
 
 export default function Footer() {
   return (
     <div>
       <div className="footerContainer">
-          <a className="github2" href="https://github.com/IanMordaunt?tab=repositories" target='_blank'>
-            <DiGithubBadge /></a>
+        <ul className="footerInfo">
+          <li className="github2"><a className="github2" href="https://github.com/IanMordaunt?tab=repositories" target="_blank"><DiGithubBadge /></a>
+          </li>
+          <li className="github3"><a className="github3" href="https://www.linkedin.com/in/ian-mordaunt/" target="_blank"><AiFillLinkedin/></a>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- react-portfolio/src/components/pages/AboutMe/AboutMe.js
+++ react-portfolio/src/components/pages/AboutMe/AboutMe.js
@@ -6,17 +6,17 @@ import Corrugated from "./corrugated.jpg";
 import { DiReact } from "react-icons/di";
 import { DiCss3Full } from "react-icons/di";
 import { DiHtml5 } from "react-icons/di";
-import { DiGit  } from 'react-icons/di';
-import { DiJsBadge  } from 'react-icons/di';
-import { DiNodejs  } from 'react-icons/di';
-import { DiNpm  } from 'react-icons/di';
-import { DiMongodb } from 'react-icons/di';
-import { DiMysql } from 'react-icons/di';
-import { DiVisualstudio } from 'react-icons/di';
-import { DiJqueryLogo } from 'react-icons/di';
-import { DiHeroku } from 'react-icons/di';
-import { DiGithubBadge } from 'react-icons/di';
-import { SiHandlebarsdotjs } from 'react-icons/si';
+import { DiGit } from "react-icons/di";
+import { DiJsBadge } from "react-icons/di";
+import { DiNodejs } from "react-icons/di";
+import { DiNpm } from "react-icons/di";
+import { DiMongodb } from "react-icons/di";
+import { DiMysql } from "react-icons/di";
+import { DiVisualstudio } from "react-icons/di";
+import { DiJqueryLogo } from "react-icons/di";
+import { DiHeroku } from "react-icons/di";
+import { DiGithubBadge } from "react-icons/di";
+import { SiHandlebarsdotjs } from "react-icons/si";
 
 export default function AboutMe() {
   return (
@@ -36,35 +36,63 @@ export default function AboutMe() {
         <h1 className="h12">- About Me -</h1>
         <p>
           Full-Stack developer living in Santa Fe, NM. Coding is my passion.
-          Developing applications that help bring ease and efficiency is the
+          Developing applications using the MERN-stack that help bring ease 
+          and efficiency is the
           focus. I have strong analytical skills for solving and finding
           solutions to complex problems. Learning something new is what I live
           for. Everyday is a new oppotunity to improve myself and grow in
           knowledge. Collaborating with others to achieve higher goals is where
-          I feel at my best. I love participating in a team environment. Making
-          progress and sharing progress with others brings me satisfaction. Life
-          long learning is my goal. Constantly gaining knowledge and putting
-          that knowledge into practice is my life goal.
+          I feel at my best. Making progress and sharing progress with others 
+          brings me satisfaction. Life long learning is my goal. Constantly gaining
+          knowledge and putting that knowledge into practice is my life goal.
         </p>
       </div>
       <div>
         <h1 className="h12">- Skills -</h1>
         <div className="skills-list text-center">
           <ul className="skills">
-            <li className="html"><DiHtml5 /></li>
-            <li className="css"><DiCss3Full /></li>
-            <li className="js"><DiJsBadge /></li>
-            <li className="node"><DiNodejs /></li>
-            <li className="npm"><DiNpm /></li>
-            <li className="jquery"><DiJqueryLogo /></li>
-            <li className="react"><DiReact /></li>
-            <li className="handlebars"><SiHandlebarsdotjs /></li>
-            <li className="mongodb"><DiMongodb /></li>
-            <li className="mysql"><DiMysql /></li>
-            <li className="git"><DiGit /></li>
-            <li className="github"><DiGithubBadge /></li>
-            <li className="heroku"><DiHeroku /></li>
-            <li className="vscode"><DiVisualstudio /></li>
+            <li className="html">
+              <DiHtml5 />
+            </li>
+            <li className="css">
+              <DiCss3Full />
+            </li>
+            <li className="js">
+              <DiJsBadge />
+            </li>
+            <li className="node">
+              <DiNodejs />
+            </li>
+            <li className="npm">
+              <DiNpm />
+            </li>
+            <li className="jquery">
+              <DiJqueryLogo />
+            </li>
+            <li className="react">
+              <DiReact />
+            </li>
+            <li className="handlebars">
+              <SiHandlebarsdotjs />
+            </li>
+            <li className="mongodb">
+              <DiMongodb />
+            </li>
+            <li className="mysql">
+              <DiMysql />
+            </li>
+            <li className="git">
+              <DiGit />
+            </li>
+            <li className="github">
+              <DiGithubBadge />
+            </li>
+            <li className="heroku">
+              <DiHeroku />
+            </li>
+            <li className="vscode">
+              <DiVisualstudio />
+            </li>
           </ul>
         </div>
       </div>
